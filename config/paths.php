103e603aa325ffbd8aa46f7c95f6dfb18ae0264b
shagrat74 <shagrat074@gmail.com>
2019-03-29 15:38:34 +0100

    -Correzione path assoluti errati -Definizione nuovi path assoluti

--- config/paths.php
+++ config/paths.php
@@ -26,36 +26,60 @@ if(is_dir(PATH.DS.'config')){
 
 // Definizione percorso file css
 if(is_dir(ASSETS.DS.'css')){
-    define('CSS', PATH.DS.'css');
+    define('CSS', ASSETS.DS.'css');
 }
 
 // Definizione percorso file js
 if(is_dir(ASSETS.DS.'js')){
-    define('JS', PATH.DS.'js');
+    define('JS', ASSETS.DS.'js');
 }
 
 // Definizione percorso file immagine
 if(is_dir(ASSETS.DS.'img')){
-    define('IMG', PATH.DS.'img');
+    define('IMG', ASSETS.DS.'img');
 }
 
 // Definizione percorso file video
 if(is_dir(ASSETS.DS.'video')){
-    define('VIDEO', PATH.DS.'video');
+    define('VIDEO', ASSETS.DS.'video');
 }
 
 // Definizione percorso fonts
 if(is_dir(ASSETS.DS.'fonts')){
-    define('FONTS', PATH.DS.'fonts');
+    define('FONTS', ASSETS.DS.'fonts');
 }
 
 // Definizione percorso file caricati
 if(is_dir(ASSETS.DS.'uploads')){
-    define('UPLOAD', PATH.DS.'uploads');
+    define('UPLOAD', ASSETS.DS.'uploads');
 }
 
 // Definizione percorso other
 if(is_dir(ASSETS.DS.'other')){
-    define('OTHER', PATH.DS.'other');
+    define('OTHER', ASSETS.DS.'other');
 }
 
+// Definizione percorso controller dell'applicazione
+if(is_dir(APP.DS.'controllers')){
+    define('CTR', APP.DS.'controllers');
+}
+
+// Definizione percorso modelli dell'applicazione
+if(is_dir(APP.DS.'models')){
+    define('MDL', APP.DS.'models');
+}
+
+// Definizione percorso viste dell'applicazione
+if(is_dir(APP.DS.'views')){
+    define('VIEWS', APP.DS.'views');
+}
+
+// Definizione percorso librerie dell'applicazione
+if(is_dir(APP.DS.'libraries')){
+    define('LIB', APP.DS.'libraries');
+}
+
+// Definizione percorso helpers dell'applicazione
+if(is_dir(APP.DS.'helpers')){
+    define('HELP', APP.DS.'helpers');
+}
